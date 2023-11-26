ead74644c76231ffa14100fcdc938d384e6f4082
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-11-26 14:35:58 +0500

    task4

---
+++ Tyuiu.TretyakovDV.Sprint5.Task4.V21/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TretyakovDV.Sprint5.Task4.V23.Lib;
+using System.IO;
+
+namespace Tyuiu.TretyakovDV.Sprint5.Task4.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Третьяков Д.В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Третьяков Денис Викторович | ПКТб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция, шаг -5;5, провести проверку деления на 0, результат       *");
+            Console.WriteLine("* сохранить в текстовый файл OutPutFileTask1.txt и вывести на консоль.    *");
+            Console.WriteLine("* Округлить до двух знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V21.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
